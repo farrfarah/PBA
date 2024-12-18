504b5d0826947397fcdfddf7451d9d4164f8d197
Putri Olivia Nuraeni <114887561+putriolivia@users.noreply.github.com>
2024-12-18 22:55:29 +0700

    Update config.py

--- BERT/config.py
+++ BERT/config.py
@@ -9,18 +9,19 @@ config = Namespace(
     test_data_path="ag_news_csv/test.csv",
     checkpoint_dir="checkpoints/model.pt",
 
+    
     # Model parameters
     enc_method="rnn",  # Encoder method
-    bert_dim=128,
-    hidden_size=8,
-    out_size=64,
-    num_labels=4,  # Number of classes in AG News
-    dropout=0.5,
+    bert_dim=768,       # Dimensi embedding
+    hidden_size=4,     # Ukuran hidden layer
+    out_size=128,       # Ukuran output layer
+    num_labels=4,      # Jumlah kelas dalam AG News
+    dropout=0.3,       # Tingkat dropout
 
     # Training parameters
-    batch_size=256,
-    learning_rate=1e-3,
-    num_epochs=5,
+    batch_size=128,     # Batch size untuk training
+    learning_rate=5e-4, # Learning rate
+    num_epochs=3,       # Jumlah epoch training
 
     # Device settings
     use_gpu=torch.cuda.is_available(),
